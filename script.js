cc489ec9a5074a8759f573f9fa3093ccc6abfac4
MeganKR <megankaye2018@gmail.com>
2023-10-10 22:12:05 -0500

    edited script.js and index.html

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var today = dayjs();
+$(".currentDay").text(today.format("MM/DD/YYYY"));
+
+// This is trying to make a variable that 
+// var student = document.getElementById("student-names");
+// var eventEntry = $('.eventInput');
+// var saveButton = $('.btn');
+
+/* $(document).ready(function () {
+  $('.btn').addEventListener("click", function () {
+    var eventEntry = $('.eventInput').value;
+    var description = $(); 
+    var hour = $(); // .??().attr()
+    localStorage.setItem(hour, description);
+    
+    // code that shows notif of item saved in local storage
+    $("").addClass("");
+    // create a set timeout to remove class after 5 seconds
+    setTimeout(function() {
+      $("").removeClass("");
+    }, 5000)
+  })
+  function updateTime() {
+    // get current time, loop through time blocks, check if time has moved past current time.
+    var date = dayjs().hour();
+  }
+  updateTime();
+  // set interval for time
+
+  // load save data in local storage
+  $(".currentDay").text(date.format("MM/DD/YYYY"));
 });
+
+
+
+// eventData.innerHTML = localStorage.getItem("value");
+
+// $(".btn").click(function() {
+// var saveText = $(event).val();
+
+// localStorage.
+// $(".eventData").innerHTML = localStorage.??
+
+
+
+// saveButton.on("click", function () {
+//  localStorage.setItem('value', input.value);
+// });
+
+/* function saveData() {
+  // Save related form data as an object
+  var eventData = {
+    eventEntry: eventEntry.value,
+  };
+  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+  localStorage.setItem('eventData', JSON.stringify(eventData));
+}
+
+function renderData() {
+  // Use JSON.parse() to convert text to JavaScript object
+  var loadEntry = JSON.parse(localStorage.getItem('eventData'));
+  // Check if data is returned, if not exit out of the function
+  if (loadEntry !== null) {
+    $('.eventInput').innerHTML = loadEntry.eventEntry;
+  }
+}
+
+saveButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  saveData();
+  renderData();
+}); */
